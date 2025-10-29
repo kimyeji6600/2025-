43894b84ccec9975ccbdc7d533ed3b81046960d4
kimyeji6600 <kimyeji6600@gmail.com>
2025-10-29 17:27:41 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.title('김예지의 첫번째 앱')
+st.subheader('배고파~~~')
+st.write('미미미미미미ㅣㅁ미ㅣ밈미')
+st.write('https://naver.com')
+st.link_button("네이버 바로가기",'https://naver.com')
+
+name=st.text_input('이름을 입력해주세요:')
+if st.button('환영인사'):
+    st.write(name+'님 안녕하세요!')
+    st.balloons()
+    st.image('https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2025/01/17/4777a40d-4cd1-4828-a0c1-a1d5bd7bdbbd.jpg')
+    st.write('하루만 네 곁에 침대가 되고 싶어~~')
+st.success('성공!')
+st.warning('경고')
+st.error('오류')
+st.info('안내문')
